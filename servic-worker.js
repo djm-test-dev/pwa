635e7722e93397a63b610657fa0023ea19421d2b
djm-test-dev <mdjonovic@gmail.com>
2025-12-01 23:52:13 +0100

    Update servic-worker.js

--- servic-worker.js
+++ servic-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'pwa-cache-v2';
 
 const FILES_TO_CACHE = [
     'index.html',
@@ -38,6 +38,7 @@ self.addEventListener('fetch', event => {
         .then(response => response || fetch(event.request))
     );
 });
+
 
 
 
